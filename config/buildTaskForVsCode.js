304a33fd0ff8d73ceba8614b5fbb869294aeac8b
MRsabS <47223291+MRsabS@users.noreply.github.com>
2020-04-24 07:29:16 +0300

    fix parcel target

--- config/buildTaskForVsCode.js
+++ config/buildTaskForVsCode.js
@@ -7,6 +7,6 @@ const entryFiles = Path.join(__dirname, '../src/server.ts');
 
 (async function() {
 
-  const bundler = new Bundler(entryFiles, {target: "electron", watch: false});
+  const bundler = new Bundler(entryFiles, {target: "node", watch: false});
   await bundler.bundle();
 })();
